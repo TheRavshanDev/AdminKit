670a3061e6669e398b9f49ec30acfd9488a72e0a
TheRavshanDev <88030420+TheRavshanDev@users.noreply.github.com>
2022-05-08 15:44:01 +0500

    Added pie chart

--- base/urls.py
+++ base/urls.py
@@ -1,13 +1,15 @@
 from ast import Add
 from django.urls import path
 from .views import (HomeView, ProfileView, AnalyticView, NotificationView, EnrolledTutorialView, 
-                    CourseDetailView, AddEnrolledCourseView, EditProfileView, EditProfileSkillsView)
+                    CourseDetailView, AddEnrolledCourseView, EditProfileView, EditProfileSkillsView,
+                    AnalyticUserView)
     
 
 urlpatterns = [
     path('dashboard/',HomeView.as_view(), name='home'),
     path('profile/',ProfileView.as_view(), name='profile'),
     path('analytic/',AnalyticView.as_view(), name='analytic'),
+    path('analytic/',AnalyticUserView.as_view(), name='analytic'),
     path('notifications/',NotificationView.as_view(), name='notifications'),
     path('tutorials/',EnrolledTutorialView.as_view(), name='tutorial'),
     path('tutorial/<int:pk>/',CourseDetailView.as_view(), name='tutorial-detail'),
